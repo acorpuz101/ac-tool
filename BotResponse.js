377d52e5a4880ef4bc4c52a7270238c57df60f67
acorpuz <adriancorpuz@gmail.com>
2021-01-18 19:49:56 -0600

    Refactored
    Separated discord bot initialization from discord bot functionality.

---
+++ BotResponse.js
@@ -0,0 +1,57 @@
+// Import dependencies
+const https = require('https');
+const fetch = require("node-fetch");
+const config = require("./config.json");
+
+const DbdApi = require('./apis/dbd/DbdApi');
+const GoogleMapsApi = require('./apis/googlemaps/GoogleMapsApi');
+const DarkSkyApi = require('./apis/darksky/DarkSkyApi');
+
+const ADRIANS_ID = '179314473088188417';
+const ANNAS_ID = '181522225835409408';
+const BOT_ID = '358256950250700805';
+const TARGET_CHANNELS = 'test,bot-cmds';
+
+let tagObj = {};
+let regex = /[#]\w*/g;
+
+module.exports = class BotResponse {
+    constructor() {
+        this.dbdApi = new DbdApi();
+        this.googleMapsApi = new GoogleMapsApi();
+        this.darkSkyApi = new DarkSkyApi();
+  }
+
+  async routeMessage(msg) {
+      // Messages from the bot itself are ignored
+      if (msg.author.id == BOT_ID || msg.author.name == 'ajon-tools') return;
+
+      // Only messages in the target channel will be replied to
+      if (!TARGET_CHANNELS.includes(msg.channel.name)) return;
+
+      let msgContent = msg.content;
+      if (msgContent === '!ping') {
+          msg.reply(config);
+      }
+      else if (msgContent.includes('!weather')) {
+          const coord = await this.googleMapsApi.getCoordinates(msgContent);
+          let data = await this.darkSkyApi.getFormattedForecast(coord.lat + "," + coord.lng, coord.formattedAddress);
+          msg.reply(
+              data
+          );
+      }
+      else if (msgContent.includes('!coord')) {
+          msg.reply(
+              await this.googleMapsApi.getFormattedCoordinates(msgContent)
+          );
+      }
+      else if (msgContent === '!dbd shrine') {
+          msg.reply(
+              await this.dbdApi.getShrine()
+          );
+      } else if (msgContent.match(regex) != null) {
+          console.log(msg);
+          msg.reply(msg.id);
+      }
+  }
+}
--- app.js
+++ app.js
@@ -1,86 +1,30 @@
+// Import dependencies
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const auth = require('./auth.json');
+
+// Import App Functions
 const HouseList = require('./house-fn/house-list');
 const SaveAppData = require('./house-fn/SaveAppData');
 const CommonUtils = require('./house-fn/CommonUtils');
-const DbdApi = require('./apis/dbd/DbdApi');
-const GoogleMapsApi = require('./apis/googlemaps/GoogleMapsApi');
-const DarkSkyApi = require('./apis/darksky/DarkSkyApi');
-const config = require("./config.json");
+const BotResponse = require('./BotResponse');
+
+
+// Import Config Files
+const auth = require('./auth.json');
+
+// Initialize Variables
+const client = new Discord.Client();
+const botResponse = new BotResponse();
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
-
-const HOUSE_SHOW_SUGGESTIONS_LIST = '!show ';
-const ADRIANS_ID = '179314473088188417';
-const ANNAS_ID = '181522225835409408';
-
-const TARGET_CHANNELS = 'test,bot-cmds';
-
-let houseList = new HouseList();
-const listSaver = new SaveAppData();
-const commonUtils = new CommonUtils();
-const dbdApi = new DbdApi();
-const googleMapsApi = new GoogleMapsApi();
-const darkSkyApi = new DarkSkyApi();
-
-let tagObj = {};
-let regex = /[#]\w*/g;
+	console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', async msg => {
-
-  // Messages from the bot itself are ignored
-  if (msg.author.id == 358256950250700805 || msg.author.name == 'ajon-tools') return;
-
-  // Only messages in the target channel will be replied to
-  if (!TARGET_CHANNELS.includes(msg.channel.name)) return;
-
-  let msgContent = msg.content;
-  if (msgContent === '!ping') {
-	  msg.reply(config);
-  }
-  else if (msgContent.includes('!weather')) {
-    const coord = await googleMapsApi.getCoordinates(msgContent);
-	let data = await darkSkyApi.getFormattedForecast(coord.lat + "," + coord.lng, coord.formattedAddress);
-	msg.reply(
-	  data
-	);
-  }
-  else if (msgContent.includes('!coord')) {
-    msg.reply(
-	  await googleMapsApi.getFormattedCoordinates(msgContent)
-	);
-  }
-  else if (msgContent === '!dbd shrine') {
-	msg.reply(
-	  await dbdApi.getShrine()
-	);
-  } else if (msgContent.match(regex) != null){
-	  console.log(msg);
-	msg.reply(msg.id);
-  }
-
-  // Save the list after each command
-  listSaver.saveListsToJson(houseList);
+    botResponse.routeMessage(msg);
  });
  
  client.on('ready', function() {
-  try {
-    const lastData = require('./liveData.json');
-    houseList = listSaver.loadData(lastData);
-  }catch(err){
-      console.log('No previous data available. Will create a new file');
-  }
 
-  try {
-   setInterval(()=>{
-     listSaver.saveListsToJson(houseList, true);
-   }, 3600000)
-  }catch(err){
-   console.log('The hourly back up save failed.');
-  }
  });
 
 client.login(auth.token);
