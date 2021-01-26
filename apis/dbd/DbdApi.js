400324db91355b0bc9b3770cfe7ba591bb9f7315
acorpuz <adriancorpuz@gmail.com>
2021-01-25 19:55:34 -0600

    Converted shrine to embed with more data

--- apis/dbd/DbdApi.js
+++ apis/dbd/DbdApi.js
@@ -1,5 +1,6 @@
 const https = require('https');
 const fetch = require("node-fetch");
+const Discord = require('discord.js');
 
 module.exports = class DbdApi {
   constructor() {
@@ -22,18 +23,48 @@ module.exports = class DbdApi {
   }
 
   async getShrine(cmd, fullCmd) {
-	this.endpoint = "/api/shrineofsecrets";
-	const data = await this.getDbdApiRequest();
-	const date = new Date(data.endDate);
-	let str = "```diff\n"  +
-		"Todays's Shine of Secrets\n" +
-		"- " + data.items[0].Name + "\n" +
-		"- " + data.items[1].Name + "\n" +
-		"- " + data.items[2].Name + "\n" +
-		"- " + data.items[3].Name + "\n" +
-		"Ends: " + date.toLocaleDateString() + " " + date.toLocaleTimeString("en-US", { timeZone: "America/Chicago" }) +" CST\n" +
-		"```";
-	return str;
+		this.endpoint = "/api/shrineofsecrets";
+		const data = await this.getDbdApiRequest();
+		const perkData = await this.getPerks();
+		return this.createShrineEmbed(data, perkData);
+	}
+
+	async getPerks() {
+		this.endpoint = "/api/perks";
+		const data = await this.getDbdApiRequest();
+		return data;
   }
+
+	// TODO: Add data for perk variables
+	cleanPerkDescriptions(rawPerkDescription) {
+
+		// Replace BR with \n for the new lines
+		let description = rawPerkDescription.replace("<br>", "\n");
+
+		// Replace HTML tags with nothing
+		description = description.replace(/(<([^>]+)>)/gi, "");
+
+		// Replace starting quotes with a new line
+		description = description.replace(" \"", "\n\"");
+		return description;
+  }
+
+	createShrineEmbed(shrineData, perkData) {
+
+		const date = new Date(shrineData.endDate);
+		const exampleEmbed = new Discord.MessageEmbed()
+			.setColor('#008061')
+			.setTitle("Today's Shrine")
+			.setDescription("Ends: " + date.toLocaleDateString() + " " + date.toLocaleTimeString("en-US", { timeZone: "America/Chicago" }) + " CST\n")
+			.addFields(
+				{ name: shrineData.items[0].Name, value: "\t" + this.cleanPerkDescriptions(perkData[shrineData.items[0].id].perkDefaultDescription) },
+				{ name: shrineData.items[1].Name, value: "\t" + this.cleanPerkDescriptions(perkData[shrineData.items[1].id].perkDefaultDescription) },
+				{ name: shrineData.items[2].Name, value: "\t" + this.cleanPerkDescriptions(perkData[shrineData.items[2].id].perkDefaultDescription) },
+				{ name: shrineData.items[3].Name, value: "\t" + this.cleanPerkDescriptions(perkData[shrineData.items[3].id].perkDefaultDescription) },
+			)
+			.setTimestamp();
+
+		return exampleEmbed;
+	}
 
 }
